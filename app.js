5772f336bb740d716ba4af7503b448afbdfd7f41
prajithor <prajithsoumithrag@gmail.com>
2026-05-14 12:58:46 +0530

    Added express logic

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+const { json } = require("express");
+
 console.log("Learning git");
 const add = (a, b) => {
     return a + b;
 }
 console.log(add(2, 4));
+addEventListener(queueMicrotask){
+    if (condition) {
+        res.json("success");
+    }
+    else{
+        res.json("happy coding");
+    }
+};
